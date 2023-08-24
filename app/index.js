7a18491bf1ac30199a3c9db2e70092cb17c2b959
MrRinkukumar <rinkukumarsaini9799118495@gmail.com>
2023-08-24 21:54:43 +0530

    the search implementation is completed

--- app/index.js
+++ app/index.js
@@ -11,18 +11,33 @@ import {
 } from "../components";
 const Home = () => {
   const router = useRouter();
+  const [searchTerm, setSearchTerm] = useState("");
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
-      <Stack.Screen options={{
-        headerStyle: { backgroundColor: COLORS.lightWhite }, headerShadowVisible: false, headerLeft: () => (
-          <ScreenHeaderBtn iconUrl={icons.menu} dimension='60%' />
-        ), headerRight: () => (
-          <ScreenHeaderBtn iconUrl={images.profile} dimension='100%' />
-        ), headerTitle: ""
-      }} />
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+          ),
+          headerTitle: "",
+        }}
+      />
       <ScrollView showsVerticalScrollIndicator={false}>
         <View style={{ flex: 1, padding: SIZES.medium }}>
-          <Welcome />
+          <Welcome
+            searchTerm={searchTerm}
+            setSearchTerm={setSearchTerm}
+            handleClick={() => {
+              if (searchTerm) {
+                router.push(`/search/${searchTerm}`);
+              }
+            }}
+          />
           <Popularjobs />
           <Nearbyjobs />
         </View>
